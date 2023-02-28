7fbad7560d6a4df1fd4af3743b186024aeac28ec
lionloopy <121242536+lionloopy@users.noreply.github.com>
2023-02-28 09:07:57 +0900

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,101 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Header from "../components/Header";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-function Login() {
+export default function App() {
+  let username = document.querySelector("#username");
+  let password = document.querySelector("#password"); //아이디 중복 확인 버튼
+  const navigate = useNavigate();
+  const [inputs, setInputs] = useState({
+    username: "",
+    password: "",
+  });
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    const nextInputs = { ...inputs, [name]: value };
+    setInputs(nextInputs);
+  };
+
+  function letsLogin() {
+    if (inputs.username === "") {
+      alert("아이디를 입력해주세요.");
+      return;
+    } else if (inputs.password === "false") {
+      alert("비밀번호가 틀렸습니다.");
+      return;
+    } else {
+      fetch("http://13.125.211.170:8080/api/user/login", {
+        //백엔드랑 협의된 주소 입력
+
+        method: "post",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          username: inputs.username,
+          password: inputs.password,
+          Authorization: inputs.Authorization,
+        }),
+      })
+        .then((res) => res.json())
+        .then((response) => {
+          // console.log(response.Authorization);
+          console.log(response.jwtUtill);
+          if (response.statusCode === "OK") {
+            // 로그인 성공시
+            alert("로그인 성공");
+            navigate("/");
+            const accessToken = response.jwtUtill;
+            console.log(accessToken);
+            // API 요청하는 콜마다 헤더에 accessToken 담아 보내도록 설정
+            axios.defaults.headers.common["jwtUtill"] = `Bearer ${accessToken}`;
+
+            window.localStorage.setItem(
+              "jwtUtill",
+              response.jwtUtill.split(" ")[2]
+            );
+            // window.location.href = "/";
+          } else {
+            // 로그인 실패시
+            alert("사용자가 존재하지 않습니다.");
+          }
+        });
+    }
+  }
+
   return (
     <Layout>
       <Header></Header>
+
+      <h2>로그인</h2>
+
+      <input
+        type="text"
+        id="username"
+        name="username"
+        placeholder="아이디"
+        onChange={onChange}
+      />
+
+      <input
+        type="password"
+        id="password"
+        name="password"
+        placeholder="비밀번호"
+        onChange={onChange}
+      />
+
+      <input
+        type="button"
+        className="button medium secondary"
+        onClick={letsLogin}
+        id="loginBtn"
+        value="로그인"
+      />
     </Layout>
   );
 }
-
-export default Login;

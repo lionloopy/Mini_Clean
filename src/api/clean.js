6a2b3f749ecba9f4acfcf296fcd3bbda40c88cb6
lionloopy <121242536+lionloopy@users.noreply.github.com>
2023-02-28 09:09:01 +0900

    Update clean.js

--- src/api/clean.js
+++ src/api/clean.js
@@ -1,11 +1,154 @@
 import axios from "axios";
+import { instance } from "./axios";
+import { baseURL } from "./axios";
+// import { getCookie } from "./cookie";
+
+// const access_token = getCookie("ACCESS_TOKEN");
+// const instance = axios.create({
+//   baseURL: `${process.env.REACT_APP_SERVER_URL}`,
+//   headers: { Authorization: `Bearer ${access_token}` },
+// });
+
+const access_token = localStorage.getItem("jwtUtill");
+const config = {
+  headers: {
+    Authorization: `Bearer ${access_token}`,
+  },
+};
+console.log(access_token, config);
 
 const getBoard = async () => {
-  const response = await axios.get(`http://localhost:4000/api`);
+  const response = await instance.get("/api/boards");
+  console.log(config);
   return response.data;
 };
+// const getBoard = async () => {
+//   const response = await instance.get("/api");
+//   return response.data;
+// };
+
 const addBoard = async (newBoard) => {
-  await axios.post(`http://localhost:4000/api`, newBoard);
+  await baseURL
+    .post("/api/board", newBoard, config)
+    .then((response) => {
+      if (response.statusCode === "OK") {
+        alert(response.msg);
+      }
+    })
+    .catch((error) => {
+      if (error.statusCode === "UNAUTHORIZED") {
+        alert(error.msg);
+      }
+    });
+};
+// const addBoard = async (newBoard) => {
+//   await baseURL
+//     .post("/api", newBoard)
+//     .then((response) => {
+//       if (response.statusCode === "OK") {
+//         alert(response.msg);
+//       }
+//     })
+//     .catch((error) => {
+//       if (error.statusCode === "UNAUTHORIZED") {
+//         alert(error.msg);
+//       }
+//     });
+// };
+
+const deleteBoard = async (id) => {
+  await baseURL
+    .delete(`/api/board/${id}`)
+    .then((response) => {
+      if (response.statusCode === "OK") {
+        alert(response.msg);
+      }
+    })
+    .catch((error) => {
+      if (error.statusCode === "UNAUTHORIZED") {
+        alert(error.msg);
+      } else if (error.statusCode === "BAD_REQUEST") {
+        alert(error.msg);
+      }
+    });
+};
+// const deleteBoard = async (id) => {
+//   await baseURL
+//     .delete(`/api/${id}`)
+//     .then((response) => {
+//       if (response.statusCode === "OK") {
+//         alert(response.msg);
+//       }
+//     })
+//     .catch((error) => {
+//       if (error.statusCode === "UNAUTHORIZED") {
+//         alert(error.msg);
+//       } else if (error.statusCode === "BAD_REQUEST") {
+//         alert(error.msg);
+//       }
+//     });
+// };
+
+const updateBoard = async (payload) => {
+  await baseURL
+    .put(`/api/board/${payload.id}`, {
+      title: payload.title,
+      images: payload.img,
+      content: payload.content,
+    })
+    .then((response) => {
+      if (response.statusCode === "OK") {
+        alert(response.msg);
+      }
+    })
+    .catch((error) => {
+      if (error.statusCode === "UNAUTHORIZED") {
+        alert(error.msg);
+      } else if (error.statusCode === "BAD_REQUEST") {
+        alert(error.msg);
+      }
+    });
 };
 
-export { getBoard, addBoard };
+// const updateBoard = async (payload) => {
+//   await axios.patch(`${process.env.REACT_APP_SERVER_URL}/api/${payload.id}`, {
+//     title: payload.title,
+//     content: payload.content,
+//     images: payload.images,
+//   });
+// };
+
+const getComment = async (id) => {
+  const response = await instance.get(`/api/comment/${id}`);
+  return response.data;
+};
+
+const addComment = async (newComment, payload) => {
+  await baseURL.post(`/api/comment/${payload.id}`, newComment);
+};
+// const addComment = async (newComment) => {
+//   await baseURL
+//     .post(`/api/comment/${newComment.target}`, newComment)
+//     .then((response) => {
+//       if (response.statusCode === "OK") {
+//         alert(response.msg);
+//       }
+//     })
+//     .catch((error) => {
+//       if (error.statusCode === "UNAUTHORIZED") {
+//         alert(error.msg);
+//       }
+//     });
+// };
+
+const deleteComment = async (id) => {
+  await axios.delete(`${process.env.REACT_APP_SERVER_URL}/api/${id}`);
+};
+export {
+  getBoard,
+  addBoard,
+  deleteBoard,
+  updateBoard,
+  addComment,
+  deleteComment,
+};
